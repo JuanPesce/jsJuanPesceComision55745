c4028087c6ee9d5d216f629d5a71a228debbfed2
Juan Pesce <juan.ignacio.pesce@gmail.com>
2023-10-19 00:03:55 -0300

    carrito vacio

--- js/funciones.js
+++ js/funciones.js
@@ -46,58 +46,11 @@ const renderProductos = async () => {
   };
   
 
-// const renderProductos = () => {
-
-//     fetch("JSON/productos.JSON")
-//     .then(respuesta => respuesta.json())
-//     .then(data => {
-//         let catalogoHTML ="";
-//         data.forEach( unidad =>{
-//             catalogoHTML +=`<div class="col-md-4 mb-5">
-//             <div class="card" style="width: 18rem;">
-//             <img src="${unidad.imagen}" class="card-img-top" alt="${unidad.nombre}">
-//             <div class="card-body">
-//               <h5 class="card-title">${unidad.nombre}</h5>
-//               <p class="card-text">$${unidad.precio}</p>
-//               <a href="#" class="btn btn-primary bg-success" onclick="agregarAlCarrito(${unidad.id})">Agregar (+)</a>
-//             </div>
-//           </div>
-//           </div>`
-//         }
-
-//         )
-//         document.getElementById("contenido").innerHTML = catalogoHTML; 
-
-//     })
-//     .catch(error => {
-//         document.getElementById("contenido").innerHTML = `<div class="alert alert-danger text-center" role="alert"><p>ERROR! No se pudo acceder a la base de datos!<br>${error}</p></div>`; 
-        
-//     })
-
-// const productos = cargarProductosLS(); 
-
-//     let contenidoHTML ="";
-    
-//     productos.forEach(producto => {
-//         contenidoHTML +=`<div class="col-md-4 mb-5">
-//         <div class="card" style="width: 18rem;">
-//         <img src="${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
-//         <div class="card-body">
-//           <h5 class="card-title">${producto.nombre}</h5>
-//           <p class="card-text">$${producto.precio}</p>
-//           <a href="#" class="btn btn-primary bg-success" onclick="agregarAlCarrito(${producto.id})">Agregar (+)</a>
-//         </div>
-//       </div>
-//       </div>`
-//     });
-
-//     document.getElementById("contenido").innerHTML = contenidoHTML; 
-// }
-
 const renderCarrito = () => {
     const productos = cargarCarritoLS(); 
-
-    let contenidoHTML =`<table class="table">`;
+    let contenidoHTML;
+    if(cantProductosCarrito()>0){
+    contenidoHTML =`<table class="table">`;
     
     productos.forEach(producto => {
         contenidoHTML +=` <tr>
@@ -116,9 +69,13 @@ const renderCarrito = () => {
     <td><b>$${sumaProductosCarrito()}</b></td>
     </tr>    
     </table>`;
-    document.getElementById("contenido").innerHTML = contenidoHTML; 
-}
+    } else {
+        contenidoHTML = `<div class="alert  alert-warning" role="alert">No se encontraron Productos en el Carrito!</div>`
+    }
 
+    document.getElementById("contenido").innerHTML = contenidoHTML; 
+
+}
 
 
 const cargarCarritoLS =() => {
